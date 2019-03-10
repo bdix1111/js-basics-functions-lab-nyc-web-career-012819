5eba3c133f953358ebd39c64ae54929a56437239
bdix1111 <brianna_dixon@rocketmail.com>
2019-03-09 20:58:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+  let distance = street - 42;
+  return Math.abs(distance)
+}
+
+function distanceFromHqInFeet(street) {
+  let distance = distanceFromHqInBlocks(street) * 264;
+  return distance
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  let distance = Math.abs(endBlock-startBlock) * 264;
+  return distance;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+  let distance = distanceTravelledInFeet(startBlock, endBlock);
+  let response;
+  if (distance <= 400) {response = 0} else if (distance > 2500) {response = 'cannot travel that far'} else if (distance <= 2000) {response = (distance - 400) * .02} else {response = 25}
+  return response;
+}
